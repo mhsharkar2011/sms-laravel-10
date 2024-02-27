b3d609546b6ef0ef95340da7c973422435b047e7
Monir Hossain <mhsharkar2011@gmail.com>
2024-02-27 13:38:35 +0600

    Dashboard Page added

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('admin/dashboard', function () {
+    return view('admin/dashboard');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
